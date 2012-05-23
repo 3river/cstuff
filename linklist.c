fbf10ac541413ae93fad652cfb2c994efef92017
3river <913185402@qq.com>
2012-05-23 10:01:49 +0800

    Update linklist.c

--- linklist.c
+++ linklist.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#incldue <stdlib.h>
+
+typedef struct node {
+	int data;
+	struct node *next;
+} Node;
+
+typedef struct list {
+	Node *head;
+} List;
+
+//--
 
 
 int main(int argc, char **argv) {
   return 0;
 }
+
